793ed02059f5ead23c828ecdf888504186b44296
T-Higashi7 <cervus--ht7578@ezweb.ne.jp>
2022-03-06 18:32:11 +0900

    不明点

--- src/Kintai.vue
+++ src/Kintai.vue
@@ -1,6 +1,7 @@
 
 <template>
-<div class="container mx-auto">
+
+<div class="container mx-auto container-fluid " style="background-color: #FFFCFB" >
   <div style="padding: 10px; margin-bottom: 10px; border: 1px dashed #333333;">
 　<p class = "title">【勤怠表入力】</p>
   <p>({{ subjectMonth }})
@@ -12,16 +13,29 @@
   </p>
   <div> 
   <p>合計勤務日数：{{ totalWork }}日</p>
-  <p>合計従業時間：{{ totalTime }}分</p>
-  <p>合計残業時間：{{ totalOverTime }}分 (内　22時以降の残業時間：{{totalMidnightOverTime}}分　　法定休日(日曜日)の残業時間：{{totalHolidayOverTime}}分)</p>
+  <p>合計従業時間：{{ totalTime }}時間 (内　22時～5時の従業時間：{{totalMidnightWorkTime}}時間)</p>
+  <p>合計残業時間：{{ totalOverTime }}時間 (内　22時～5時の残業時間：{{totalMidnightOverTime}}時間　　法定休日(日曜日)の残業時間：{{totalHolidayOverTime}}時間)</p>
   <p>　　当直回数：{{ totalOndutyWork }}回</p>
+  <p>　　夜勤回数：{{ totalNightWork }}回</p>
   <p>　　有給時間：{{ totalPaidTime }}分</p>
   </div>
-  </div>~
+  </div>
+      <div class="table-responsive">
          <table class="table">
            <thead thead class="table-light">
            <tr>
-                <th>日付</th><th>曜日</th><th>勤務</th><th>勤務開始時間</th><th>勤務終了時間</th><th>従業時間</th><th>昼休憩</th><th>残業時間</th><th>22時～5時の残業</th><th>残業理由・その他備考</th><th>有給時間</th>
+                <th scope="col" style="background-color: #EEEEEE">日付</th>
+                <th scope="col" style="background-color: #EEEEEE">曜日</th>
+                <th scope="col" style="background-color: #CCFFCC">勤務</th>
+                <th scope="col" style="background-color: #CCFFCC">勤務開始</th>
+                <th scope="col" style="background-color: #CCFFCC">勤務終了</th>
+                <th scope="col" style="background-color: #CCFFCC">従業時間</th>
+                <th scope="col" style="background-color: #CCFFCC">22時～5時の従業</th>
+                <th scope="col" style="background-color: #CCFFCC">昼休憩</th>
+                <th scope="col" style="background-color: #CCFFFF">残業時間</th>
+                <th scope="col" style="background-color: #CCFFFF">22時～5時の残業</th>
+                <th scope="col" style="background-color: #CCFFFF">残業理由・備考</th>
+                <th scope="col" style="background-color: #FFFF99">有給</th>
            </tr>
            </thead>
            <tbody>
@@ -33,6 +47,7 @@
                  <select v-model="element.editWork">
                       <option value=〇>〇</option>
                       <option value=〇/当>〇/当</option>
+                      <option value=深夜>深夜</option>
                       <option value=A/公>A/公</option>
                       <option value=公/A>公/A</option>
                       <option value=A/有>A/有</option>
@@ -55,18 +70,27 @@
                   </select>
                 </td>
                 <td v-if="element.show">{{searchTimeListValue(element.endtime)}}</td>
-                <td v-if="!element.show">
+                <!-- 夜勤区分がfalseの場合-->
+                <td v-if="!element.show && !element.nightWork">
                    <select v-model="element.editEndTime" >
                       <option v-for="time in timeList" :value="time.value">
                       {{time.text}}
                       </option>
                    </select>
                 </td>
+                <!-- 夜勤区分がtrueの場合-->
+                <td v-if="!element.show && element.nightWork">
+                   <select v-model="element.editEndTime" >
+                      <option v-for="time in timeList" :value="time.nightWorkValue">
+                      {{time.text}}
+                      </option>
+                   </select>
+                </td>
                 <td>{{element.workTime}}</td>
+                <td>{{element.midnightWorkTime}}</td>
                 <td>{{element.restTime}}</td>
                 <td>{{element.overtime}}</td>
                 <td>{{element.midnightOvertime}}</td>
-
                 <td v-if="element.show">{{element.reason}}</td>
                 <td v-if="!element.show">
                  <input v-model="element.editReason">
@@ -92,101 +116,102 @@
                       <option value=480>480</option>
                     </select>
                   </td>
-                <td v-if="element.show"><button class="btn btn-outline-dark" v-on:click="edit(element)" >編集</button>
-                </td>
-                <td v-if="element.show"><button class="btn btn-outline-dark" v-on:click="deleate(element);totalWorkday()" >削除</button>
-                </td>
-                <td v-if="!element.show">
-                <button class="btn btn-outline-dark" v-on:click="confirm(element);total()" >確定</button>
-                </td>
-                <td v-if="!element.show">
-                <button class="btn btn-outline-dark" v-on:click="cancel(element)" >キャンセル</button>
-                </td>
+                <td v-if="element.show"><button class="btn btn-outline-dark" v-on:click="edit(element)" >編集</button></td>
+                <td v-if="element.show"><button class="btn btn-outline-dark" v-on:click="deleate(element);totalWorkday()" >削除</button></td>
+                <td v-if="!element.show"><button class="btn btn-outline-dark" v-on:click="confirm(element);total()" >確定</button></td>
+                <td v-if="!element.show"><button class="btn btn-outline-dark" v-on:click="cancel(element)" >キャンセル</button></td>
 
-                  <div v-if="element.show">
-                  </div>
+              <div v-if="element.show">
+              </div>
 
 　　         </tr>
             </tbody>
          </table>
+     </div>
   </div>
 
 </template>
 
 <script>
 //同じ階層にあるkintai.jsを読み込む。
-import { workTime, overtime, midnightOvertime } from "./kintai";
+import { workTime, overTime, midnightOvertime, midnightWorkTime } from "./kintai";
 
 export default {
   data() {
     return {
       current: 0,
       meisaiList: null,
+      
+      //勤務が深夜の場合は勤務終了時間にnightWorkValueを使用する。
       timeList:[
-        {id : 1, value:495, text: "8:15" },
-        {id : 2, value:525, text: "8:45" },
-        {id : 3, value:555, text: "9:15" },
-        {id : 4, value:585, text: "9:45" },
-        {id : 5, value:615, text: "10:15" },
-        {id : 6, value:645, text: "10:45" },
-        {id : 7, value:675, text: "11:15" },
-        {id : 8, value:705, text: "11:45" },
-        {id : 9, value:735, text: "12:15" },
-        {id : 10, value:765, text: "12:45" },
-        {id : 11, value:795, text: "13:15" },
-        {id : 12, value:825, text: "13:45" },
-        {id : 13, value:855, text: "14:15" },
-        {id : 14, value:885, text: "14:45" },
-        {id : 15, value:915, text: "15:15" },
-        {id : 16, value:945, text: "15:45" },
-        {id : 17, value:975, text: "16:15" },
-        {id : 18, value:1005, text: "16:45" },
-        {id : 19, value:1035, text: "17:15" },
-        {id : 20, value:1065, text: "17:45" },
-        {id : 21, value:1095, text: "18:15" },
-        {id : 22, value:1125, text: "18:45" },
-        {id : 23, value:1155, text: "19:15" },
-        {id : 24, value:1185, text: "19:45" },
-        {id : 25, value:1215, text: "20:15" },
-        {id : 26, value:1245, text: "20:45" },
-        {id : 27, value:1275, text: "21:15" },
-        {id : 28, value:1305, text: "21:45" },
-        {id : 29, value:1335, text: "22:15" },
-        {id : 30, value:1365, text: "22:45" },
-        {id : 31, value:1395, text: "23:15" },
-        {id : 32, value:1425, text: "23:45" },
-        {id : 33, value:1455, text: "00:15" },
-        {id : 34, value:1485, text: "00:45" },
-        {id : 35, value:1515, text: "1:15" },
-        {id : 36, value:1545, text: "1:45" },
-        {id : 37, value:1575, text: "2:15" },
-        {id : 38, value:1605, text: "2:45" },
-        {id : 39, value:1635, text: "3:15" },
-        {id : 40, value:1665, text: "3:45" },
-        {id : 41, value:1695, text: "4:15" },
-        {id : 42, value:1725, text: "4:45" },
-        {id : 43, value:1755, text: "5:15" },
-        {id : 44, value:1785, text: "5:45" },
-        {id : 45, value:1815, text: "6:15" },
-        {id : 46, value:1845, text: "6:45" },
-        {id : 47, value:1875, text: "7:15" },
-        {id : 48, value:1905, text: "7:45" },
-        {id : 49, value:1920, text: "8:00" },
+        {id : 1, value:"8:15", nightWorkValue:"32:15", text: "8:15" },
+        {id : 2, value:"8:45", nightWorkValue:"32:45", text: "8:45" },
+        {id : 3, value:"9:15", nightWorkValue:"33:15", text: "9:15" },
+        {id : 4, value:"9:45", nightWorkValue:"33:45", text: "9:45" },
+        {id : 5, value:"10:15", nightWorkValue:"34:15", text: "10:15" },
+        {id : 6, value:"10:45", nightWorkValue:"34:45", text: "10:45" },
+        {id : 7, value:"11:15", nightWorkValue:"35:15", text: "11:15" },
+        {id : 8, value:"11:45", nightWorkValue:"35:45", text: "11:45" },
+        {id : 9, value:"12:15", nightWorkValue:"36:15", text: "12:15" },
+        {id : 10, value:"12:45", nightWorkValue:"36:45", text: "12:45" },
+        {id : 11, value:"13:15", nightWorkValue:"13:15", text: "13:15" },
+        {id : 12, value:"13:45", nightWorkValue:"13:45", text: "13:45" },
+        {id : 13, value:"14:15", nightWorkValue:"14:15", text: "14:15" },
+        {id : 14, value:"14:45", nightWorkValue:"14:45", text: "14:45" },
+        {id : 15, value:"15:15", nightWorkValue:"15:15", text: "15:15" },
+        {id : 16, value:"15:45", nightWorkValue:"15:45", text: "15:45" },
+        {id : 17, value:"16:15", nightWorkValue:"16:15", text: "16:15" },
+        {id : 18, value:"16:45", nightWorkValue:"16:45", text: "16:45" },
+        {id : 19, value:"17:15", nightWorkValue:"17:15", text: "17:15" },
+        {id : 20, value:"17:45", nightWorkValue:"17:45", text: "17:45" },
+        {id : 21, value:"18:15", nightWorkValue:"18:15", text: "18:15" },
+        {id : 22, value:"18:45", nightWorkValue:"18:45", text: "18:45" },
+        {id : 23, value:"19:15", nightWorkValue:"19:15", text: "19:15" },
+        {id : 24, value:"19:45", nightWorkValue:"19:45", text: "19:45" },
+        {id : 25, value:"20:15", nightWorkValue:"20:15", text: "20:15" },
+        {id : 26, value:"20:45", nightWorkValue:"20:45", text: "20:45" },
+        {id : 27, value:"21:15", nightWorkValue:"21:15", text: "21:15" },
+        {id : 28, value:"21:45", nightWorkValue:"21:45", text: "21:45" },
+        {id : 29, value:"22:15", nightWorkValue:"22:15", text: "22:15" },
+        {id : 30, value:"22:45", nightWorkValue:"22:45", text: "22:45" },
+        {id : 31, value:"23:15", nightWorkValue:"23:15", text: "23:15" },
+        {id : 32, value:"23:45", nightWorkValue:"23:45", text: "23:45" },
+        {id : 33, value:"24:15", nightWorkValue:"24:15", text: "00:15" },
+        {id : 34, value:"24:45", nightWorkValue:"24:45", text: "00:45" },
+        {id : 35, value:"25:15", nightWorkValue:"25:15", text: "1:15" },
+        {id : 36, value:"25:45", nightWorkValue:"25:45", text: "1:45" },
+        {id : 37, value:"26:15", nightWorkValue:"26:15", text: "2:15" },
+        {id : 38, value:"26:45", nightWorkValue:"26:45", text: "2:45" },
+        {id : 39, value:"27:15", nightWorkValue:"27:15", text: "3:15" },
+        {id : 40, value:"27:45", nightWorkValue:"27:45", text: "3:45" },
+        {id : 41, value:"28:15", nightWorkValue:"28:15", text: "4:15" },
+        {id : 42, value:"28:45", nightWorkValue:"28:45", text: "4:45" },
+        {id : 43, value:"29:15", nightWorkValue:"29:15", text: "5:15" },
+        {id : 44, value:"29:45", nightWorkValue:"29:45", text: "5:45" },
+        {id : 45, value:"30:15", nightWorkValue:"30:15", text: "6:15" },
+        {id : 46, value:"30:45", nightWorkValue:"30:45", text: "6:45" },
+        {id : 47, value:"31:15", nightWorkValue:"31:15", text: "7:15" },
+        {id : 48, value:"31:45", nightWorkValue:"31:45", text: "7:45" },
+        {id : 49, value:"32:00", nightWorkValue:"32:00", text: "8:00" },
       ],
       //勤務日数の合計
       totalWork: 0,
       //従業時間の合計
       totalTime: 0,
+      //22時～5時にかかる従業時間の合計
+      totalMidnightWorkTime: 0,
       //残業時間の合計
       totalOverTime: 0,
       //有給時間の合計
       totalPaidTime: 0,
-      //22時以降の残業時間の合計
+      //22時～5時にかかる残業時間の合計
       totalMidnightOverTime: 0,
       //法定休日の残業時間の合計
       totalHolidayOverTime: 0,
       //当直の合計
       totalOndutyWork: 0,
+      //夜勤の合計
+      totalNightWork: 0,
     };
   },
 
@@ -211,15 +236,18 @@ export default {
     //「編集」ボタンを押した時に明細を編集する。
     edit: function (selectedElement) {
       selectedElement.editWork = selectedElement.work;
+      console.log(selectedElement.nightWork)
       selectedElement.editStartTime = selectedElement.starttime;
       selectedElement.editEndTime = selectedElement.endtime;
       selectedElement.editWorkTime = selectedElement.workTime;
+      selectedElement.editMidnightWorkTime = selectedElement.midnightWorkTime;
       selectedElement.editRestTime = selectedElement.restTime;
       selectedElement.editOverTime = selectedElement.overtime;
       selectedElement.editMidnightOverTime = selectedElement.midnightOvertime;
       selectedElement.editReason = selectedElement.reason;
       selectedElement.editPaidTime = selectedElement.paidTime;
       selectedElement.show = false;
+      //selectedElement.nightWork = false;
     },
 
     //「確定」ボタンを押した時の動作
@@ -228,23 +256,44 @@ export default {
       selectedElement.starttime = selectedElement.editStartTime;
       selectedElement.endtime = selectedElement.editEndTime;
       const work = selectedElement.work;
+      if(work == "深夜"){
+        selectedElement.nightWork = true;
+      } else {
+        selectedElement.nightWork = false;
+      }
       const starttime = selectedElement.starttime;
       const endtime = selectedElement.endtime;
+      const start = starttime.split(":");
+      const startTimeMinute = Number(start[0] * 60) + Number(start[1]);
+      const end = endtime.split(":");
+      const endTimeMinute = Number(end[0] * 60) + Number(end[1]);
+
       //明細の従業時間を自動算出する。
-      const worktime = workTime(starttime, endtime, work);
-      selectedElement.workTime = worktime;
+      selectedElement.workTime = workTime(starttime, endtime, work)/60;
+      //明細の22時～5時にかかる従業時間を自動算出する
+      selectedElement.midnightWorkTime =  midnightWorkTime(starttime, endtime)/60;
+      //勤務開始22:15	勤務終了5:15等、明細の22時～5時にかかる従業時間が従業時間を超えていた場合を考慮する。
+      if(selectedElement.midnightWorkTime > selectedElement.workTime){
+        selectedElement.midnightWorkTime = selectedElement.workTime/60;
+      }
+
       //明細の休憩時間を自動算出する。
-      if (selectedElement.workTime >= 450) {
-        selectedElement.restTime = 60;
+      if (endTimeMinute - startTimeMinute >= 480) {
+        selectedElement.restTime = "60分";
+      } else if(endTimeMinute - startTimeMinute < 480 && endTimeMinute - startTimeMinute >= 360) {
+        selectedElement.restTime = "45分";
       } else {
         selectedElement.restTime = 0;
       }
       //明細の残業時間を自動算出する。
-      const over = overtime(starttime, endtime, work);
-      selectedElement.overtime = over;
-      //明細の22時以降の残業時間を自動算出する。
+      selectedElement.overtime = overTime(starttime, endtime, work)/60;
+      //明細の22時～5時にかかる残業時間を自動算出する。
       const midnightOver = midnightOvertime(starttime, endtime, work);
-      selectedElement.midnightOvertime = midnightOver;
+      selectedElement.midnightOvertime = midnightOver/60;
+    　//20時15分から6時15分まで勤務した場合、22時から5時にかかる残業時間が-15になるので0になるようにする。
+      if(selectedElement.midnightOvertime<0){
+        selectedElement.midnightOvertime = 0
+      }
       //明細で編集した残業理由を残業理由に代入する。
       selectedElement.reason = selectedElement.editReason;
       selectedElement.paidTime = selectedElement.editPaidTime;
@@ -259,12 +308,14 @@ export default {
       selectedElement.editStartTime = "";
       selectedElement.editEndTime = "";
       selectedElement.editworkTime = "";
+      selectedElement.editmidnightWorkTime = "";
       selectedElement.editRestTime = "";
       selectedElement.editOverTime = "";
       selectedElement.editMidnightOverTime = "";
       selectedElement.editReason = "";
       selectedElement.editPaidTime = "";
       selectedElement.show = true;
+      selectedElement.nightWork = false;
     },
 
     //「削除」ボタンを押した時に日付と曜日以外の列の値を空データにする。
@@ -273,6 +324,7 @@ export default {
       element.starttime = "";
       element.endtime = "";
       element.workTime = "";
+      element.midnightWorkTime = "";
       element.restTime = "";
       element.overtime = "";
       element.midnightOvertime = "";
@@ -285,8 +337,14 @@ export default {
     subMission: function(){
       const a = confirm("提出しますか？");
       console.log(a)
-      if(""){
-        console.log("こんにちは")
+      if(a==true){
+        const url = "http://127.0.0.1:8000/api/submission";
+        //エンドポイントにクエリパラメータでgetでリクエストを送信する。
+         axios.post(url,this.meisaiList)
+        //送信結果をレスポンスという引数で受け取る。
+         .then((response) => {
+          console.log(response) 
+        });
       } else {
         console.log("こんばんは")
       }
@@ -299,7 +357,8 @@ export default {
       for (let i = 0; i < this.calendarData; i++) {
         if (
           this.meisaiList[i].work == "〇" ||
-          this.meisaiList[i].work == "〇/当"
+          this.meisaiList[i].work == "〇/当"||
+          this.meisaiList[i].work == "深夜"
         ) {
           this.totalWork = this.totalWork + 1;
         } else if (
@@ -326,6 +385,18 @@ export default {
       }
     },
 
+    //合計の22時～5時にかかる従業時間を計算する。
+    totalWorkMidnightWorkTime: function () {
+      this.totalMidnightWorkTime = 0;
+      for (let i = 0; i < this.calendarData; i++) {
+        if (this.meisaiList[i].midnightWorkTime == "") {
+          this.totalMidnightWorkTime = this.totalMidnightWorkTime + 0;
+        } else {
+          this.totalMidnightWorkTime = this.totalMidnightWorkTime + Number(this.meisaiList[i].midnightWorkTime);
+         }
+        }
+      },
+
     //合計残業時間を計算する。
     totalWorkOverTime: function () {
       this.totalOverTime = 0;
@@ -338,16 +409,14 @@ export default {
       }
     },
 
-    //合計の22時以降の残業を計算する。
+    //合計の22時～5時にかかる残業を計算する。
     totalWorkMidnightOverTime: function () {
       this.totalMidnightOverTime = 0;
       for (let i = 0; i < this.calendarData; i++) {
         if (this.meisaiList[i].midnightOvertime == "") {
           this.totalMidnightOverTime = this.totalMidnightOverTime + 0;
         } else {
-          this.totalMidnightOverTime =
-            this.totalMidnightOverTime +
-            Number(this.meisaiList[i].midnightOvertime);
+          this.totalMidnightOverTime =this.totalMidnightOverTime + Number(this.meisaiList[i].midnightOvertime);
         }
       }
     },
@@ -404,6 +473,7 @@ export default {
     //それぞれの合計を算出
     total: function () {
       this.totalWorktime();
+      this.totalWorkMidnightWorkTime();
       this.totalWorkday();
       this.totalWorkOverTime();
       this.totalWorkMidnightOverTime();
@@ -411,6 +481,7 @@ export default {
       this.totalWorkOverTimeHoliday();
       this.totalPaid();
     },
+
   },
 
   computed: {
@@ -473,17 +544,22 @@ export default {
           Calendar.isSunday = false;
         }
         Calendar.work = "";
+        Calendar.nightWork = "";
         Calendar.starttime = "";
         Calendar.endtime = "";
         Calendar.workTime = "";
+        Calendar.midnightWorkTime = "";
         Calendar.restTime = "";
         Calendar.overtime = "";
         Calendar.midnightOvertime = "";
         Calendar.reason = "";
         Calendar.paidTime = "";
         Calendar.editWork = "";
+        Calendar.editNightWork = "";
         Calendar.editStartTime = "";
         Calendar.editEndTime = "";
+        Calendar.editWorkTime = "";
+        Calendar.editMidnightWorkTime = "";
         Calendar.editRestTime = "";
         Calendar.editOverTime = "";
         Calendar.editMidnightOvertime = "";
@@ -524,12 +600,17 @@ export default {
   color: #0000ff;
 }
 
-td,
-th {
+.container-fluid {
+max-width: 1400px;
+}
+
+td {
   text-align: center;
 }
 
-th {
-  text-align: center;
+tr {
+  position: sticky;
+  top: 0; /* Don't forget this, required for the stickiness */
 }
+
 </style>
--- src/kintai.js
+++ src/kintai.js
@@ -4,14 +4,19 @@ function workTime(starttime, endtime, work) {
     if (work == "公" || work == "有給" || work == "特休" || work == "振休" || work == "欠勤") {
         worktime = 0;
     } else {
-        // const start = starttime.split(":");
-        //const startTimeMinute = Number(start[0] * 60) + Number(start[1]);
-        const startTimeMinute = starttime;
-        //const end = endtime.split(":");
-        //const endTimeMinute = Number(end[0] * 60) + Number(end[1]);
-        const endTimeMinute = endtime;
-        if (endTimeMinute - startTimeMinute >= 450) {
+        const start = starttime.split(":");
+        const startTimeMinute = Number(start[0] * 60) + Number(start[1]);
+        const end = endtime.split(":");
+        const endTimeMinute = Number(end[0] * 60) + Number(end[1]);
+
+        //勤務時間が8時間(480分)以上の場合は
+        if (endTimeMinute - startTimeMinute >= 480) {
+            //勤務時間は60分休憩を引いた時間となる。
             worktime = (endTimeMinute - startTimeMinute) - 60;
+            //勤務時間が6時間(360分)以上、8時間(480分)未満の場合は
+        } else if(endTimeMinute - startTimeMinute < 480 && endTimeMinute - startTimeMinute >= 360  ) {
+            //勤務時間は45分休憩を引いた時間となる。
+            worktime = (endTimeMinute - startTimeMinute) - 45;
         } else {
             worktime = endTimeMinute - startTimeMinute;
         }
@@ -22,39 +27,92 @@ function workTime(starttime, endtime, work) {
 
 //明細の残業時間を求める。日勤の場合かつ、1日の従業時間が休憩時間を含めて9時間(540分)より長いの場合はその分を残業とする。
 //半日の場合は1日の従業時間が休憩時間を含めて5時間(300分)より長い場合はその分を残業とする。
-function overtime(starttime, endtime, work) {
-    let overTime = 0;
-    const startTimeMinute = starttime;
-    const endTimeMinute = endtime;
+function overTime(starttime, endtime, work) {
+    let over = 0;
+    const start = starttime.split(":");
+    const startTimeMinute = Number(start[0] * 60) + Number(start[1]);
+    const end = endtime.split(":");
+    const endTimeMinute = Number(end[0] * 60) + Number(end[1]);
     const totalTime = endTimeMinute - startTimeMinute;
     if ((work == "〇" || work == "〇/当") && totalTime > 540) {
-        overTime = totalTime - 540;
+        over = totalTime - 540;
     } else if ((work == "A/公" || work == "公/A" || work == "有/A" || work == "A/有") && totalTime > 300) {
-        overTime = totalTime - 300;
+        over = totalTime - 300;
     } else {
-        overTime = 0;
+        over = 0;
     }
-    return overTime;
+    return over;
+}
+//従業時間が22時から5時にかかる時間を求める。
+function midnightWorkTime(starttime, endtime) {
+    let midnightWorkTime = 0;
+    const start = starttime.split(":");
+    const startTimeMinute = Number(start[0] * 60) + Number(start[1]);
+    const end = endtime.split(":");
+    const endTimeMinute = Number(end[0] * 60) + Number(end[1]);
+    const totalTime = endTimeMinute - startTimeMinute;
+    
+    if (Number(end[0])>=22 && Number(end[0])>=29) {
+        //勤務終了時間がAM5時(60*29 = 1740)を超えた時間をoverFiveとする。
+        const overFive = endTimeMinute - 1740;
+        //勤務開始時間からAM22時(60*22 = 1320)を超えなかった時間をnotOverTenとする。
+        let notOverTen = 1320 - startTimeMinute;
+        //勤務開始時間がAM22時(60*22 = 1320)を超えていた場合
+        if(notOverTen < 0){
+            // 従業時間から勤務終了時間がAM5時(60*29 = 1740)を超えた時間を引く。
+            midnightWorkTime = totalTime - overFive;
+        } else {
+            // 従業時間から勤務終了時間がAM5時(60*29 = 1740)を超えた時間と勤務開始時間ががAM22時(60*22 = 1320)を超えなかった時間を引く。
+            midnightWorkTime = totalTime - (overFive + notOverTen);
+        }
+    } else if((Number(end[0])>=22 && Number(end[0])<29)){
+        //勤務開始時間からAM22時(60*22 = 1320)を超えなかった時間をnotOverTenとする。
+        const notOverTen = 1320 - startTimeMinute;
+        midnightWorkTime = totalTime - notOverTen;
+    } else {
+        //それ以外の場合は深夜従業時間は0となる。
+        midnightWorkTime = 0;
+    }
+    return midnightWorkTime;
 }
 
 //残業時間(従業時間が540分以上になる時間)が22時から5時にかかる時間を求める。
 function midnightOvertime(starttime, endtime) {
+    //深夜残業時間の初期値は0とする。
     let midnightOvertime = 0;
-    //勤務開始時間をstartTimeMinuteとする。
-    const startTimeMinute = starttime;
-    //勤務終了時間をstartTimeMinuteとする。
-    const endTimeMinute = endtime;
+    const start = starttime.split(":");
+    const startTimeMinute = Number(start[0] * 60) + Number(start[1]);
+    const end = endtime.split(":");
+    const endTimeMinute = Number(end[0] * 60) + Number(end[1]);
     //従業時間をtotalTimeとする。
     const totalTime = endTimeMinute - startTimeMinute;
-    //従業時間が540分を過ぎた残業時間をoverTimeとする。
-    const overTime = totalTime - 540;
-    //22時以降、5時未満の時間はendTimeMinute > 1320 && endTimeMinute < 1740で表すことができる？
-    if (endTimeMinute > 1320 && endTimeMinute < 1740) {
-        
+    //残業の基準となる時間をoverStandardTime(勤務開始時間+540分)とする。overStandardTimeを超えた時間は残業扱い。
+    const overStandardTime = startTimeMinute+540;
+    //従業時間が540分を超えるかつ勤務終了時間が22時以降かつ5時を超える場合
+    if (totalTime >540 && Number(end[0])>=22 && Number(end[0])>=29) {
+        //勤務終了時間がAM5時(60*29 = 1740)を超えた時間をoverFiveとする。
+        const overFive = endTimeMinute - 1740;
+        //深夜の残業時間は勤務終了時間-22時(60*22=1320)-5時となる。
+        midnightOvertime = (endTimeMinute - 1320)-overFive;
+    } 
+    　//従業時間が540分を超えるかつ勤務終了時間が22時以降かつ5時を超えない場合
+    　else if(totalTime >540 && Number(end[0])>=22 && Number(end[0])<29) {
+        //深夜の残業時間は勤務終了時間-22時(60*22=1320)-となる。
+        midnightOvertime = endTimeMinute - 1320;
     } else {
+        //それ以外の場合は深夜残業時間は0分となる。
         midnightOvertime = 0;
     }
-    return midnightOvertime;
+　　
+    //深夜残業時間が0より上かつ、残業の基準となる時間(勤務開始時間+540分)が22時を超える場合は
+    if(midnightOvertime > 0 && overStandardTime > 1320){
+        //深夜残業時間は深夜残業時間-(残業時間の基準となる時間-22時)となる。
+        return midnightOvertime-(overStandardTime - 1320);
+    }
+    else {
+        //それ以外の場合は深夜残業時間をそのまま返す。
+        return midnightOvertime;
+    }
 }
 
-export { workTime, overtime, midnightOvertime };
+export { workTime, overTime, midnightOvertime, midnightWorkTime};
